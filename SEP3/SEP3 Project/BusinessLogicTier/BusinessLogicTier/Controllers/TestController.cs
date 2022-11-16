d2452d26d03d0f4b019e43683c6e22d43c25dc04
Matas Armonaitis <matas.armonaitis@gmail.com>
2022-11-16 12:04:54 +0100

    BusinessLogicTier
    Test controller updates

--- SEP3/SEP3 Project/BusinessLogicTier/BusinessLogicTier/Controllers/TestController.cs
+++ SEP3/SEP3 Project/BusinessLogicTier/BusinessLogicTier/Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers;
@@ -13,5 +14,30 @@ public class TestController : ControllerBase
     {
         return Ok("This was accepted as authorized");
     }
-    
+    [HttpGet("allowanon"), AllowAnonymous]
+    public ActionResult GetAsAnon()
+    {
+        return Ok("This was accepted as anonymous");
+    }
+    [HttpGet("mustbeBuyer"), Authorize("MustBeBuyer")]
+    public ActionResult GetAsBuyer()
+    {
+        return Ok("This was accepted as buyer domain");
+    }
+    [HttpGet("manualcheck")]
+    public ActionResult GetWithManualCheck()
+    {
+        Claim? claim = User.Claims.FirstOrDefault(claim => claim.Type.Equals(ClaimTypes.Role));
+        if (claim == null)
+        {
+            return StatusCode(403, "You have no role");
+        }
+
+        if (!claim.Value.Equals("Seller"))
+        {
+            return StatusCode(403, "You are not a seller");
+        }
+
+        return Ok("You are a Seller, you may proceed");
+    }
 }
