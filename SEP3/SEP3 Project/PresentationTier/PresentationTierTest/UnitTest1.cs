d5cf705829dd72f90826ffd3b18efd4a52630d1b
Matas Armonaitis <matas.armonaitis@gmail.com>
2022-11-29 18:41:50 +0100

    Test for Creating posts
    The test for creating posts is created

--- SEP3/SEP3 Project/PresentationTier/PresentationTierTest/UnitTest1.cs
+++ SEP3/SEP3 Project/PresentationTier/PresentationTierTest/UnitTest1.cs
@@ -1,3 +1,4 @@
+using AngleSharp.Dom;
 using AutoFixture;
 using AutoFixture.NUnit3;
 using BlazorWASM.Pages;
@@ -6,9 +7,11 @@ using BlazorWasm.Services.Http;
 using Bunit;
 using Bunit.TestDoubles;
 using Domain.DTOs;
+using Domain.Models;
 using HttpClients.ClientInterfaces;
 using HttpClients.Implementations;
 using Microsoft.Extensions.DependencyInjection;
+using RichardSzalay.MockHttp;
 
 namespace PresentationTierTest;
 
@@ -24,7 +27,7 @@ public class UnitTest1 : TestContext
       var cut = ctx.RenderComponent<CreateUser>();
       var fixture = new Fixture();
       var strings = fixture.Create<List<string>>();
-      UserCreationDto userCreationDto = new UserCreationDto(strings[0],strings[1],strings[2],strings[3],strings[4],true);
+      UserCreationDto userCreationDto = new UserCreationDto("","","","","",true);
       Services.AddTransient<IUserService, UserHttpClient>();
       var api = Services.GetRequiredService<IUserService>();
 
@@ -36,12 +39,11 @@ public class UnitTest1 : TestContext
       cut.Find("#email input").Change("Danila@gmail.com");
       cut.Find("#password input").Change("pepega");
       cut.Find("#phoneNumber input").Change("+4512345678");
-      cut.Find("#create Button").Click();
+      
       */
 
       //Assert
 
-      
     }
 
     [Fact]
@@ -63,7 +65,8 @@ public class UnitTest1 : TestContext
 
         api.LoginAsync(strings[0], strings[1]);
         //Assert
-        cut.Markup.Contains(@"<div><label>Error: String reference not set to an instance of a String. (Parameter 's')</label></div>");
+        cut.MarkupMatches(@"");
+
     }
     [Fact]
     public void UserShouldNotBeAbleToLogIn()
@@ -90,6 +93,33 @@ public class UnitTest1 : TestContext
         */
         cut.Find("Button").Click();
         //Assert
-        cut.Markup.Contains(@"<div><label>Error: String reference not set to an instance of a String. (Parameter 's')</label></div>");
+        var contains = cut.Markup.Contains(@"Error: String reference not set to an instance of a String. (Parameter 's')");
+        Assert.True(contains);
+    }
+
+    [Fact]
+    public void PostIsCreated()
+    {
+        //Arrange
+        using var ctx = new TestContext();
+        var mock = ctx.Services.AddMockHttpClient();
+        ctx.Services.AddSingleton<IItemService>(new ItemHttpClient(new HttpClient()));
+        ctx.Services.AddSingleton<IUserService>(new UserHttpClient(new HttpClient()));
+        var authContext = ctx.AddTestAuthorization();
+        mock.When("/Item").RespondJson(new List<Item>());
+        authContext.SetAuthorized("Test User");
+        var cut = ctx.RenderComponent<CreatePost>();
+        var fixture = new Fixture();
+        var strings = fixture.Create<List<string>>();
+        Services.AddTransient<IItemService, ItemHttpClient>();
+        var api = Services.GetRequiredService<IItemService>();
+        ItemCreationDto item = new ItemCreationDto();
+        
+
+        //Act
+        api.Create(item);
+        //Assert
+        var contains = cut.Markup.Contains(@"<h4></h4>");
+        Assert.True(contains);
     }
 }
