f1b928ccf0ecbbb29e2e185d7ba638cb2123f137
Matas Armonaitis <matas.armonaitis@gmail.com>
2022-12-07 18:36:54 +0100

    Tests for Creating/getting items and for getting users complete

--- SEP3/SEP3 Project/BusinessLogicTier/TestBusinessLogicTier/UnitTest1.cs
+++ SEP3/SEP3 Project/BusinessLogicTier/TestBusinessLogicTier/UnitTest1.cs
@@ -1,70 +1,91 @@
-using Application.Logic;
-using Application.LogicInterfaces;
-using Domain.DTOs;
-using Domain.Models;
-using FakeItEasy;
-using FileData.DAOs;
 using Grpc.Core;
-using Grpc.Core.Testing;
-using Microsoft.AspNetCore.Mvc;
-using WebAPI.Controllers;
+using Grpc.Net.Client;
+using gRPCClient;
+using User = Domain.Models.User;
+using Item = Domain.Models.Item;
 
 namespace TestBusinessLogicTier;
 
 public class Tests
 {
+    //This test class only works with the database server online
+    [Fact]
+    public async Task GetUser_ShouldReturnUser()
+    {
+        // Arrange
+        GrpcChannel channel = GrpcChannel.ForAddress("http://localhost:6565");
+        UserService.UserServiceClient sut = new UserService.UserServiceClient(channel);
+        List<User> users = new();
+        // Act
+        var call = sut.getUsers(new SearchUserDTO
+        {
+            Id = 1,
+            FirstName = "",
+            LastName = ""
+        });
+        await foreach (var response in call.ResponseStream.ReadAllAsync())
+        {
+            User? user = new()
+            {
+                Id = (int)response.Id,
+                firstName = response.FirstName,
+                lastName = response.LastName,
+                email = response.Email,
+                gender = response.Gender,
+                password = "",
+                phoneNumber = response.PhoneNumber,
+                dor = new(response.DateOfRegistration.Year, response.DateOfRegistration.Month,
+                    response.DateOfRegistration.Day,
+                    response.DateOfRegistration.Hour, response.DateOfRegistration.Minute, 0)
+            };
+            users.Add(user);
+        }
+        // Assert
+        Assert.Equal("Dan-Sebastian", users[1].firstName);
+            
+    }
 
     [Fact]
-    public async void Test1()
+    public async void CreateItemAndGetItem_ShouldCreateAnItem_ShouldBeAbleToSeeGetTheItem()
     {
         //Arrange
+        GrpcChannel channel = GrpcChannel.ForAddress("http://localhost:6565");
+        ItemService.ItemServiceClient? ClientItem = new ItemService.ItemServiceClient(channel);
 
-        int count = 1;
-        // var fakeUsers = A.CollectionOfDummy<SearchUserParametersDto>(count).AsEnumerable();\
-        var fakeUsers = A.CollectionOfDummy<User>(count).AsEnumerable();
-        User user = A.Fake<User>();
-        List<User> users = A.Fake<List<User>>();
-        users.Add(user);
-        var fakeFirstName = "First";
-        var fakeLastName = "Second";
-        var userLogic = A.Fake<UserLogic>();
-        var dataStore = A.Fake<UserFileDao>();
-        var searchParameters = A.Fake<SearchUserParametersDto>();
-        A.CallTo(() => dataStore.GetAsync(searchParameters)).Returns(Task.FromResult(users));
-        var controller = new UsersController(userLogic);
-        
+
         //Act
-        var actionResult = await controller.GetAsync(fakeFirstName, fakeLastName);
+        await ClientItem.createItemAsync(new CreateItemDTO
+        {
+            OwnerId = 1,
+            Category = "Electronics",
+            Currency = "Euro",
+            Description = "Stuff",
+            Name = "Stuff",
+            Price = 500,
+            Status = false
+        });
+        List<Item> items = new();
+        
+        var call = ClientItem.getItems(new SearchItemDTO() { Name = "Stuff",Description = "Stuff",Id = 3, Category = "Electronics",
+            MaxPrice = 500,MinPrice = 500,OwnerId = 1,Status = false});
+        await foreach (var response in call.ResponseStream.ReadAllAsync())
+        { 
+            Item? toSend = new()
+            {
+                Id = (int)response.Id,
+                Category = response.Category,
+                Pricing = response.Price,
+                OwnerId = (int)response.Owner.Id,
+                Currency = response.Currency,
+                Description = response.Description,
+                IsSold = response.Status,
+                Name = response.Name
+            };
+            items.Add(toSend);
+        }
         
         //Assert
-        var result = actionResult.Result as OkObjectResult;
-        var returnUsers = result.Value as IEnumerable<User>;
-        Assert.Equal(count, returnUsers.Count());
-
-
+        Assert.Equal("Stuff", items[0].Name);
     }
-    [Fact]
-    public async Task Call_Grpc_Method_Test()
-    {
-        var grpcService = _service.GetRequiredService<IMyService>();
-
-        var request = new MyRequest {When=DateTime.Now.AddDays(30)};
     
-        var context = TestServerCallContext.Create(
-            method: nameof(IMyService.WinningLotteryNumbers)
-            , host: "localhost"
-            , deadline: DateTime.Now.AddMinutes(30)
-            , requestHeaders: new Metadata()
-            , cancellationToken: CancellationToken.None
-            , peer: "10.0.0.25:5001"
-            , authContext: null
-            , contextPropagationToken: null
-            , writeHeadersFunc: (metadata) => Task.CompletedTask
-            , writeOptionsGetter: () => new WriteOptions()
-            , writeOptionsSetter: (writeOptions) => { }
-        ) ;
-        var answer= await grpcService.WinningLotteryNumbers(request, context);
-        Assert.IsNotNull(answer);
-
-    }
 }
