7c5eb439430f9dc4f4ec9bfad9ce282d5970a35e
YoUnGi102 <90184551+YoUnGi102@users.noreply.github.com>
2022-12-10 13:02:46 +0100

    Revert "Added test for dataacces"
    This reverts commit 2667fe1d072b8454f7cadf43ed2f43651a083131.

--- SEP3/SEP3 Project/DataAccessTier/src/main/java/dk/via/nbnp/databaseserver/domain/User.java
+++ SEP3/SEP3 Project/DataAccessTier/src/main/java/dk/via/nbnp/databaseserver/domain/User.java
@@ -2,14 +2,12 @@ package dk.via.nbnp.databaseserver.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@JsonPOJOBuilder
 @Table(name="\"user\"")
 public class User {
 
