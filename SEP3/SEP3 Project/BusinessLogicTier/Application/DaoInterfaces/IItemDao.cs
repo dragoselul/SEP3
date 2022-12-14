7c19fa72320749b1356fb11910bb378f5a63a1b2
YoUnGi102 <90184551+YoUnGi102@users.noreply.github.com>
2022-12-14 17:00:49 +0100

    Documentation Update

--- SEP3/SEP3 Project/BusinessLogicTier/Application/DaoInterfaces/IItemDao.cs
+++ SEP3/SEP3 Project/BusinessLogicTier/Application/DaoInterfaces/IItemDao.cs
@@ -6,12 +6,9 @@ namespace Application.DaoInterfaces;
 public interface IItemDao
 {
     Task<Item> CreateAsync(Item post);
-    
     Task<List<Item>> GetAsync(SearchItemParametersDto searchParameters);
     Task<List<Item>> GetAllItemsAsync();
     Task<Item> UpdateAsync(Item post);
-
     Task<Item> GetByIdAsync(int id);
-    
     Task DeleteItemById(int id);
 }
